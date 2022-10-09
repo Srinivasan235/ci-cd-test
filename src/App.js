1c39a81bf0af83e285e877f413d568532ee93678
unknown <srinivasan.rajagopal00@gmail.com>
2022-10-09 20:35:39 +0530

    ci cd test

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Jenkins Test v2 push<code>src/App.js</code> and save to reload.
+          Jenkins Test v2 push 
         </p>
+        <h3>CI CD is awesome</h3>
         <a
           className="App-link"
           href="https://reactjs.org"
